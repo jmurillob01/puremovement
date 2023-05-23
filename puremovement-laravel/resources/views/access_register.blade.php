145dfa9bb38a0d2b851f04b71e28ee99336763cd
Javi <javiermb2302@gmail.com>
2023-05-23 11:28:57 +0200

    Cambios en diseño

--- puremovement-laravel/resources/views/access_register.blade.php
+++ puremovement-laravel/resources/views/access_register.blade.php
@@ -6,45 +6,45 @@ registro
 
 @section('content')
 <div id="access-content" class="container">
-    <form name="fRegisterUser" id="register-form" class="row" action="{{route('user.store')}}" method="POST"> <!-- class = was-validated -->
+    <form name="fRegisterUser" id="register-form" class="form row" action="{{route('user.store')}}" method="POST"> <!-- class = was-validated -->
         @csrf
 
-        <div class="col-12 col-md-6">
+        <div class="col-md-6 col-12">
             <label for="user-name" class="form-label">Id Usuario</label>
             <input type="text" class="form-control" id="user-id" name="id" title="Permite letras, números y '_' Longitud 4-20" value="{{old('id')}}" pattern="^[a-zA-Z0-9_]{4,20}$" required>
             <!-- <div id="id-feedback" class="is-invalid-div container"></div> -->
         </div>
 
-        <div class="col-12 col-md-6">
+        <div class="col-md-6 col-12">
             <label for="user-password" class="form-label">Contraseña</label>
             <input type="password" class="form-control" id="user-password" name="password" title="Permite letras, números y símbolos '_.!¿?¡' Longitud 5-20" pattern="^[A-z0-9_.!¿?¡]{5,20}$" required>
             <!-- <div id="password-feedback" class="is-invalid-div container"></div> -->
         </div>
 
-        <div class="col-12 col-md-4">
+        <div class="col-md-4 col-12">
             <label for="user-name" class="form-label">Nombre</label>
             <input type="text" class="form-control" id="user-name" name="name" value="{{old('name')}}" pattern="^[ a-zA-Záéíóúäëïöüàèìòù]{3,20}$" required>
             <!-- <div id="name-feedback" class="is-invalid-div container"></div> -->
         </div>
 
-        <div class="col-12 col-md-4">
+        <div class="col-md-4 col-12">
             <label for="user-lastname1" class="form-label">Primer apellido</label>
             <input type="text" class="form-control" id="user-lastname1" name="lastname1" value="{{old('lastname1')}}" pattern="^[ a-zA-Záéíóúäëïöüàèìòù]{3,20}$" required>
             <!-- <div id="lastname1-feedback" class="is-invalid-div container"></div> -->
         </div>
 
-        <div class="col-12 col-md-4">
+        <div class="col-md-4 col-12">
             <label for="user-lastname2" class="form-label">Segundo apellido</label>
             <input type="text" class="form-control" id="user-lastname2" name="lastname2" value="{{old('lastname2')}}" pattern="^[ a-zA-Záéíóúäëïöüàèìòù]{3,20}$" required>
             <!-- <div id="lastname2-feedback" class="is-invalid-div container"></div> -->
         </div>
 
-        <div class="col-12 col-md-6">
+        <div class="col-md-6 col-12">
             <label for="user-phone" class="form-label">Teléfono</label>
             <input type="tel" class="form-control" id="user-phone" name="phone" value="{{old('phone')}}" pattern="^[0-9]{9}$" required>
         </div>
 
-        <div class="col-12 col-md-6">
+        <div class="col-md-6 col-12">
             <label for="user-email" class="form-label">Correo electrónico</label>
             <input type="email" class="form-control" id="user-email" name="email" value="{{old('email')}}" required>
         </div>
@@ -90,7 +90,7 @@ registro
                 </div> -->
 
         <div id="access-submit" class="mb-3">
-            <button class="btn btn-primary" type="submit">Registrar</button>
+            <button class="btn btn-info" type="submit">Registrar</button>
         </div>
     </form>
 </div>
--- puremovement-laravel/resources/views/principal.blade.php
+++ puremovement-laravel/resources/views/principal.blade.php
@@ -37,50 +37,6 @@
             </div>
         </div>
 
-        <!-- Modal IMC INFO -->
-        <div class="modal fade" id="imc-modal" tabindex="-1" aria-labelledby="imcModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="imcModalLabel"></h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Para adultos de 20 años o más, el IMC se interpreta usando categorías de estado de peso estándar. Estas categorías son iguales para hombres y mujeres de todos los tipos de cuerpo y edades.</p>
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th scope="col">IMC</th>
-                                    <th scope="col">Nivel de peso</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Por debajo de 18.5</td>
-                                    <td>Bajo peso</td>
-                                </tr>
-                                <tr>
-                                    <td>18.5 – 24.9</td>
-                                    <td>Normal</td>
-                                </tr>
-                                <tr>
-                                    <td>25.0 – 29.9</td>
-                                    <td>Sobrepeso</td>
-                                </tr>
-                                <tr>
-                                    <td>30.0 o más</td>
-                                    <td>Obesidad</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <!-- Nav -->
         <nav id="navbar-navbar" class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
@@ -90,10 +46,11 @@
 
                 <!-- Icono temporal de la página class="navbar-brand -->
                 <div class="navbar-brand">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bezier" viewBox="0 0 16 16">
+                    <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bezier" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
                         <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z" />
-                    </svg>
+                    </svg> -->
+                    <img class="logo" src="{{url('src/logo.png')}}" alt="logo-puremovement">
                     <button class="navbar-buttom" onclick="window.location.assign('/')">Puremovement</button>
                 </div>
 
@@ -115,32 +72,32 @@
         <div id="carouselExample" class="carousel slide">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{url('src/Carrousel.jpg')}}" class="d-block w-100" alt="...">
+                    <img src="{{url('src/banner1.jpg')}}" class="d-block w-100" alt="...">
                 </div>
-                <div class="carousel-item">
+                <!-- <div class="carousel-item">
                     <img src="{{url('src/Carrousel_2.jpg')}}" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
                     <img src="{{url('src/Carrousel_3.jpg')}}" class="d-block w-100" alt="...">
-                </div>
+                </div> -->
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
-            </button>
+            </button> -->
         </div>
 
         <!-- Por esto se ve mal en móvil. Si pongo container se arregla pero se fastidia la vista -->
         <div id="div-graphic-calculate-buttons" class="container">
-            <div id="div-buttons" class="div-buttons row"> <!-- row col-12 col-md-6 -->
+             <!-- <div id="div-buttons" class="div-buttons row">  row col-12 col-md-6
                 <button type="button" id="my-recipes" class="btn btn-primary btn-principal btn-box btn-access col-6 col-md-4">Mis recetas</button>
                 <button type="button" id="user-management" class="btn btn-primary btn-principal btn-box btn-access col-6 col-md-4">Gestión Usuario</button>
                 <button type="button" id="update-data" class="btn btn-primary btn-principal btn-box btn-access col-6 col-md-4">Actualizar Datos</button>
-            </div>
+            </div> -->
         </div>
 
         <!-- <div id="div-recipe-ranking" class="row container">
@@ -151,8 +108,13 @@
             </div>
         </div> -->
     </main>
-    <footer>
-
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-white text-muted">
+        <!-- Copyright -->
+        <div class="footer-container copyright text-center p-4">
+            © 2023 Copyright: 
+            <a class="text-reset fw-bold"> Javier Murillo Burgos</a>
+        </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
